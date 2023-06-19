6891fda4f33bc6e62c6bb6bb59e5bf81f989d673
Olena Jerdeva <lena.jerdeva28@gmail.com>
2023-06-19 13:56:49 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+      elements: { email, password },
+    } = event.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+      alert('Пожалуйста заполните все поля');
+    } else if (email.value === '' && password.value === '') {
+      alert('Пожалуйста заполните все поля');
+    } else {
+      console.log(`Email: ${email.value}, Password: ${password.value}`);
+      event.currentTarget.reset();
+    }
+}
