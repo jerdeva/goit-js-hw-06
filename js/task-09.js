a5efa56e122c0ff029fb794c57ac4b1ed39b51e9
Olena Jerdeva <lena.jerdeva28@gmail.com>
2023-06-19 20:00:36 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const spanColor = document.querySelector('.color');
+const btnChangeColor = document.querySelector('.change-color');
+const bodyColor =document.querySelector('body')
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+};
+
+const changeColor = () => {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = bodyColor.style.backgroundColor;
+
 }
+btnChangeColor.addEventListener('click', changeColor);
